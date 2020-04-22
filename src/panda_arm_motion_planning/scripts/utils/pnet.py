7be74845e7137b6a2021ff2400bd3a657a2ce38a
rajathkmanjunath <rajath.km@nyu.edu>
2020-04-22 12:51:20 -0400

    Making changes in the encoder network training files.
    Signed-off-by: rajathkmanjunath <rajath.km@nyu.edu>

--- src/panda_arm_motion_planning/scripts/utils/pnet.py
+++ src/panda_arm_motion_planning/scripts/utils/pnet.py
@@ -5,14 +5,21 @@ class PNet(nn.Module):
     def __init__(self, input_size, output_size):
         super(PNet, self).__init__()
         self.fulcon = nn.Sequential(nn.Linear(input_size, 1280), nn.PReLU(), nn.Dropout(),
+                                    nn.Linear(1280, 1280), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(1280, 1024), nn.PReLU(), nn.Dropout(),
+                                    nn.Linear(1024, 1024), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(1024, 896), nn.PReLU(), nn.Dropout(),
+                                    nn.Linear(896, 896), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(896, 768), nn.PReLU(), nn.Dropout(),
+                                    nn.Linear(768, 768), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(768, 512), nn.PReLU(), nn.Dropout(),
+                                    nn.Linear(512, 512), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(512, 384), nn.PReLU(), nn.Dropout(),
+                                    nn.Linear(384, 384), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(384, 256), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(256, 256), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(256, 128), nn.PReLU(), nn.Dropout(),
+                                    nn.Linear(128, 128), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(128, 64), nn.PReLU(), nn.Dropout(),
                                     nn.Linear(64, 32), nn.PReLU(),
                                     nn.Linear(32, output_size))
